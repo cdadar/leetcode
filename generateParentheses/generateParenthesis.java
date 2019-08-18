11f6f456ed6ddbfd54b9106f1ca89023e5e6b0dd
cdadar <gunbanmi@gmail.com>
2019-08-19 00:37:01 +0800

    update 22. 括号生成

--- generateParentheses/generateParenthesis.java
+++ generateParentheses/generateParenthesis.java
@@ -5,39 +5,26 @@ import java.util.List;
 
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> combinations = new ArrayList<>();
-        this.generateAll(new char[2 * n], 0, combinations);
-        return combinations;
+        List<String> ans = new ArrayList<>();
+        this.backtrack(ans, "", 0, 0, n);
+        return ans;
     }
 
-    private void generateAll(char[] current, int pos, List<String> result) {
-        if (pos == current.length) {
-            if (this.valid(current)) {
-                result.add(new String(current));
-            }
-        } else {
-            current[pos] = '(';
-            this.generateAll(current, pos + 1, result);
-            current[pos] = ')';
-            this.generateAll(current, pos + 1, result);
+    private void backtrack(List<String> ans, String current, int open, int close, int max) {
+        if (current.length() == 2 * max) {
+            ans.add(current);
+            return;
+        }
+
+        if (open < max) {
+            this.backtrack(ans, current + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            this.backtrack(ans, current + ")", open, close + 1, max);
         }
 
     }
 
-    private boolean valid(char[] current) {
-        int balance = 0;
-        for (char c : current) {
-            if (c == '(') {
-                balance++;
-            } else {
-                balance--;
-            }
-            if (balance < 0) {
-                return false;
-            }
-        }
-        return (balance == 0);
-    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
