ecb9e0c4244cb4ed4f586bba77444d2eb46e9595
cdadar <gunbanmi@gmail.com>
2019-08-26 00:13:41 +0800

    add question 23. 合并K个排序链表

---
+++ mergeKSortedLists/mergeKLists.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) { val = x; }
+}
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
