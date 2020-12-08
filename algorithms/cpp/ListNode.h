1873cb923b5b85e4a5db4e88a7929bdf50531603
chens <chens@fangde.com>
2020-12-08 14:25:40 +0800

    update struct cpp

--- algorithms/cpp/ListNode.h
+++ algorithms/cpp/ListNode.h
@@ -1,24 +1,11 @@
 #ifndef _LISTNODE_H
 #define _LISTNODE_H
 struct ListNode {
-
     int val;
-
-    struct ListNode* next;
-
-    ListNode(){
-        val = 0;
-        next = nullptr;
-    }
-    ListNode(int x) {
-        val = x;
-        next = nullptr;
-    }
-
-    ListNode(int x, ListNode* next){
-        val = x;
-        next =next;
-    }
+    struct ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 #endif
