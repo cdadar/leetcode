40e0c87bc989b0892a7f9a0faff6abc864976ea9
chens <chens@fangde.com>
2020-12-15 15:29:27 +0800

    update implement strStr of java

---
+++ algorithms/java/src/main/java/implementStrstr/Solution1.java
@@ -0,0 +1,15 @@
+package implementStrstr;
+
+public class Solution1 {
+
+  public int strStr(String haystack, String needle) {
+    int n = haystack.length();
+    int l = needle.length();
+    for (int start = 0; start < n - l + 1; start++) {
+      if (haystack.substring(start, start + l).equals(needle)) {
+        return start;
+      }
+    }
+    return -1;
+  }
+}
